3844b580d52c0c17581925b7ca5d4be791a31032
Nezar Abdennur <nabdennur@gmail.com>
2025-10-21 12:15:56 -0400

    Split pybigtools lib.rs into submodules (#91)
    * refactor: Split pybigtools lib into submodules
    
    * Rename test_bigwig.py to test_io.py
    
    * Add python-requires and PEP 735 dependency groups
    
    * ci: Use uv and dependency groups
    
    * Add development documentation to pybt readme
    
    * Add uv lockfile
    
    * Fix typo
    
    * Remove non_snake_case macro
    
    * Add cache keys for uv to rebuild automatically
    
    * Update readme
    
    * Update pyproject.toml
    
    * Update gitignore

---
+++ pybigtools/src/errors.rs
@@ -0,0 +1,46 @@
+use pyo3::prelude::*;
+use pyo3::{create_exception, exceptions};
+
+create_exception!(
+    pybigtools,
+    BBIFileClosed,
+    exceptions::PyException,
+    "BBI File is closed."
+);
+create_exception!(
+    pybigtools,
+    BBIReadError,
+    exceptions::PyException,
+    "Error reading BBI file."
+);
+
+trait ToPyErr {
+    fn to_py_err(self) -> PyErr;
+}
+
+impl ToPyErr for bigtools::BBIReadError {
+    fn to_py_err(self) -> PyErr {
+        PyErr::new::<BBIReadError, _>(format!("{}", self))
+    }
+}
+impl ToPyErr for bigtools::ZoomIntervalError {
+    fn to_py_err(self) -> PyErr {
+        match self {
+            bigtools::ZoomIntervalError::ReductionLevelNotFound => {
+                PyErr::new::<exceptions::PyKeyError, _>(format!(
+                    "The passed reduction level was not found"
+                ))
+            }
+            _ => PyErr::new::<BBIReadError, _>(format!("{}", self)),
+        }
+    }
+}
+
+pub trait ConvertResult<T> {
+    fn convert_err(self) -> Result<T, PyErr>;
+}
+impl<T, E: ToPyErr> ConvertResult<T> for Result<T, E> {
+    fn convert_err(self) -> Result<T, PyErr> {
+        self.map_err(|e| e.to_py_err())
+    }
+}
